3cfe1c7f092b253cb02eed944c71cf52d3832fe2
Amin Razavi <amin.razavi@mintlayer.org>
2022-09-19 19:55:21 +0400

    update axum-core version for request-body-size security issue

--- src/chain/nft.rs
+++ src/chain/nft.rs
@@ -1,11 +1,15 @@
 use crate::chain::chain::get_nft_data;
 use async_trait::async_trait;
 use axum::{extract, response::IntoResponse};
-use std::fs::create_dir_all;
-use std::io::{Read, Write};
+use std::{
+	fs::create_dir_all,
+	io::{Read, Write},
+};
 
-use subxt::ext::sp_core::{sr25519, ByteArray, Pair};
-use subxt::ext::sp_runtime::AccountId32;
+use subxt::ext::{
+	sp_core::{sr25519, ByteArray, Pair},
+	sp_runtime::AccountId32,
+};
 //use crate::chain::chain::ternoa::runtime_types::sp_core::crypto::AccountId32;
 
 //use subxt::ext::sp_core::crypto::Ss58C&&odec;
@@ -16,313 +20,311 @@ const NFT_DIR_PATH: &str = "./credentials/nft/";
 
 #[derive(Debug)]
 pub enum SecretError {
-    DecodeError,
-    SignatureInvalid,
-    OwnerInvalid,
+	DecodeError,
+	SignatureInvalid,
+	OwnerInvalid,
 }
 
 #[derive(Deserialize, Clone)]
 pub struct SecretPost {
-    account_address: AccountId32,
-    secret_data: Vec<u8>,
-    signature: sr25519::Signature,
+	account_address: AccountId32,
+	secret_data: Vec<u8>,
+	signature: sr25519::Signature,
 }
 
 #[derive(Serialize)]
 pub struct SecretPostResponse {
-    status: u32,
-    cluster_id: u32,
-    description: String,
+	status: u32,
+	cluster_id: u32,
+	description: String,
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct SecretData {
-    nft_id: u32,
-    data: Vec<u8>,
+	nft_id: u32,
+	data: Vec<u8>,
 }
 
 #[derive(Deserialize)]
 pub struct SecretGet {
-    account_address: AccountId32,
-    data: Vec<u8>,
-    signature: sr25519::Signature,
+	account_address: AccountId32,
+	data: Vec<u8>,
+	signature: sr25519::Signature,
 }
 
 #[derive(Serialize)]
 pub struct SecretGetResponse {
-    status: u32,
-    cluster_id: u32,
-    secret_data: SecretData,
+	status: u32,
+	cluster_id: u32,
+	secret_data: SecretData,
 }
 
 pub enum NFTOwner {
-    Owner(AccountId32),
-    NotFound,
+	Owner(AccountId32),
+	NotFound,
 }
 
 pub async fn get_nft_owner(nft_id: u32) -> NFTOwner {
-    let data = get_nft_data(nft_id).await;
+	let data = get_nft_data(nft_id).await;
 
-    let owner = match data {
-        Some(nft_data) => NFTOwner::Owner(nft_data.owner),
-        None => NFTOwner::NotFound,
-    };
+	let owner = match data {
+		Some(nft_data) => NFTOwner::Owner(nft_data.owner),
+		None => NFTOwner::NotFound,
+	};
 
-    owner
+	owner
 }
 
 #[async_trait]
 pub trait VerifyNFT {
-    fn from_bytes(body: Vec<u8>) -> Self;
-    fn get_public_key(&self) -> sr25519::Public;
-    fn verify_signature(&self) -> bool;
-    async fn check_nft_ownership(&self) -> bool;
-    async fn verify_receive_data(&self) -> Result<SecretData, SecretError>;
+	fn from_bytes(body: Vec<u8>) -> Self;
+	fn get_public_key(&self) -> sr25519::Public;
+	fn verify_signature(&self) -> bool;
+	async fn check_nft_ownership(&self) -> bool;
+	async fn verify_receive_data(&self) -> Result<SecretData, SecretError>;
 }
 
 #[async_trait]
 impl VerifyNFT for SecretPost {
-    fn from_bytes(body: Vec<u8>) -> SecretPost {
-        let decoded_secret_data: SecretPost = deserialize(&body).unwrap();
-        decoded_secret_data
-    }
+	fn from_bytes(body: Vec<u8>) -> SecretPost {
+		let decoded_secret_data: SecretPost = deserialize(&body).unwrap();
+		decoded_secret_data
+	}
 
-    fn get_public_key(&self) -> sr25519::Public {
-        sr25519::Public::from_slice(self.account_address.clone().as_slice()).expect("Valid address")
-    }
+	fn get_public_key(&self) -> sr25519::Public {
+		sr25519::Public::from_slice(self.account_address.clone().as_slice()).expect("Valid address")
+	}
 
-    fn verify_signature(&self) -> bool {
-        let account_pubkey = self.get_public_key();
-        sr25519::Pair::verify(
-            &self.signature.clone(),
-            self.secret_data.clone(),
-            &account_pubkey,
-        )
-    }
+	fn verify_signature(&self) -> bool {
+		let account_pubkey = self.get_public_key();
+		sr25519::Pair::verify(&self.signature.clone(), self.secret_data.clone(), &account_pubkey)
+	}
 
-    async fn check_nft_ownership(&self) -> bool {
-        let decoded_secret_data: SecretData = deserialize(&self.secret_data.clone()).unwrap();
-        let nft_id = decoded_secret_data.nft_id;
-        let nft_owner = get_nft_owner(nft_id).await;
-        match nft_owner {
-            NFTOwner::Owner(owner) => owner == self.account_address,
-            NFTOwner::NotFound => false,
-        }
-    }
+	async fn check_nft_ownership(&self) -> bool {
+		let decoded_secret_data: SecretData = deserialize(&self.secret_data.clone()).unwrap();
+		let nft_id = decoded_secret_data.nft_id;
+		let nft_owner = get_nft_owner(nft_id).await;
+		match nft_owner {
+			NFTOwner::Owner(owner) => owner == self.account_address,
+			NFTOwner::NotFound => false,
+		}
+	}
 
-    async fn verify_receive_data(&self) -> Result<SecretData, SecretError> {
-        if self.verify_signature() {
-            if self.check_nft_ownership().await {
-                let decoded_secret_data: SecretData = deserialize(&self.secret_data).unwrap();
-                Ok(decoded_secret_data)
-            } else {
-                Err(SecretError::OwnerInvalid)
-            }
-        } else {
-            Err(SecretError::SignatureInvalid)
-        }
-    }
+	async fn verify_receive_data(&self) -> Result<SecretData, SecretError> {
+		if self.verify_signature() {
+			if self.check_nft_ownership().await {
+				let decoded_secret_data: SecretData = deserialize(&self.secret_data).unwrap();
+				Ok(decoded_secret_data)
+			} else {
+				Err(SecretError::OwnerInvalid)
+			}
+		} else {
+			Err(SecretError::SignatureInvalid)
+		}
+	}
 }
 
 #[async_trait]
 impl VerifyNFT for SecretGet {
-    fn from_bytes(body: Vec<u8>) -> Self {
-        let req: SecretGet = deserialize(&body).unwrap();
-        req
-    }
+	fn from_bytes(body: Vec<u8>) -> Self {
+		let req: SecretGet = deserialize(&body).unwrap();
+		req
+	}
 
-    fn get_public_key(&self) -> sr25519::Public {
-        sr25519::Public::from_slice(self.account_address.clone().as_slice()).expect("Valid address")
-    }
+	fn get_public_key(&self) -> sr25519::Public {
+		sr25519::Public::from_slice(self.account_address.clone().as_slice()).expect("Valid address")
+	}
 
-    fn verify_signature(&self) -> bool {
-        let account_pubkey = self.get_public_key();
-        sr25519::Pair::verify(&self.signature.clone(), &self.data, &account_pubkey)
-    }
+	fn verify_signature(&self) -> bool {
+		let account_pubkey = self.get_public_key();
+		sr25519::Pair::verify(&self.signature.clone(), &self.data, &account_pubkey)
+	}
 
-    async fn check_nft_ownership(&self) -> bool {
-        let secret_data: SecretData = deserialize(&self.data).unwrap();
-        let nft_owner = get_nft_owner(secret_data.nft_id).await;
-        match nft_owner {
-            NFTOwner::Owner(owner) => owner == self.account_address,
-            NFTOwner::NotFound => false,
-        }
-    }
+	async fn check_nft_ownership(&self) -> bool {
+		let secret_data: SecretData = deserialize(&self.data).unwrap();
+		let nft_owner = get_nft_owner(secret_data.nft_id).await;
+		match nft_owner {
+			NFTOwner::Owner(owner) => owner == self.account_address,
+			NFTOwner::NotFound => false,
+		}
+	}
 
-    async fn verify_receive_data(&self) -> Result<SecretData, SecretError> {
-        if self.verify_signature() {
-            if self.check_nft_ownership().await {
-                let secret_data: SecretData = deserialize(&self.data).unwrap();
-                Ok(secret_data)
-            } else {
-                Err(SecretError::OwnerInvalid)
-            }
-        } else {
-            Err(SecretError::SignatureInvalid)
-        }
-    }
+	async fn verify_receive_data(&self) -> Result<SecretData, SecretError> {
+		if self.verify_signature() {
+			if self.check_nft_ownership().await {
+				let secret_data: SecretData = deserialize(&self.data).unwrap();
+				Ok(secret_data)
+			} else {
+				Err(SecretError::OwnerInvalid)
+			}
+		} else {
+			Err(SecretError::SignatureInvalid)
+		}
+	}
 }
 
 /* POST SECRET */
 pub async fn store_secret_shares(
-    extract::Json(received_secret): extract::Json<SecretPost>,
+	extract::Json(received_secret): extract::Json<SecretPost>,
 ) -> impl IntoResponse {
-    let verified_secret = received_secret.verify_receive_data().await;
+	let verified_secret = received_secret.verify_receive_data().await;
 
-    match verified_secret {
-        Ok(secret) => {
-            create_dir_all(NFT_DIR_PATH).unwrap();
-            let file_path = NFT_DIR_PATH.to_owned() + &secret.nft_id.to_string() + ".secret";
-            let mut f = std::fs::File::create(file_path).expect("create secret nft file, failed");
-            f.write_all(&secret.data).unwrap();
-            
-            println!("Secret is successfully stored to TEE, nft_id = {} by Owner = {}", secret.nft_id, received_secret.account_address);
+	match verified_secret {
+		Ok(secret) => {
+			create_dir_all(NFT_DIR_PATH).unwrap();
+			let file_path = NFT_DIR_PATH.to_owned() + &secret.nft_id.to_string() + ".secret";
+			let mut f = std::fs::File::create(file_path).expect("create secret nft file, failed");
+			f.write_all(&secret.data).unwrap();
 
-            return axum::Json(SecretPostResponse {
-                status: 200,
-                cluster_id: 1,
-                description: "Secret is successfully stored to TEE".to_string(),
-            });
-        }
+			println!(
+				"Secret is successfully stored to TEE, nft_id = {} by Owner = {}",
+				secret.nft_id, received_secret.account_address
+			);
 
-        Err(err) => match err {
-            SecretError::SignatureInvalid => {
-                println!("Error storing secrets to TEE : Invalid Request Signature");
+			return axum::Json(SecretPostResponse {
+				status: 200,
+				cluster_id: 1,
+				description: "Secret is successfully stored to TEE".to_string(),
+			})
+		},
 
-                return axum::Json(SecretPostResponse {
-                    status: 400,
-                    cluster_id: 1,
-                    description: "Error storing secrets to TEE : Invalid Request Signature"
-                        .to_string(),
-                })
-            }
+		Err(err) => match err {
+			SecretError::SignatureInvalid => {
+				println!("Error storing secrets to TEE : Invalid Request Signature");
 
-            SecretError::OwnerInvalid => {
-                println!("Error storing secrets to TEE : Invalid NFT Owner");
+				return axum::Json(SecretPostResponse {
+					status: 400,
+					cluster_id: 1,
+					description: "Error storing secrets to TEE : Invalid Request Signature"
+						.to_string(),
+				})
+			},
 
-                return axum::Json(SecretPostResponse {
-                    status: 401,
-                    cluster_id: 1,
-                    description: "Error storing secrets to TEE : Invalid NFT Owner".to_string(),
-                })
-            }
+			SecretError::OwnerInvalid => {
+				println!("Error storing secrets to TEE : Invalid NFT Owner");
 
-            SecretError::DecodeError => todo!(),
-        },
-    }
+				return axum::Json(SecretPostResponse {
+					status: 401,
+					cluster_id: 1,
+					description: "Error storing secrets to TEE : Invalid NFT Owner".to_string(),
+				})
+			},
+
+			SecretError::DecodeError => todo!(),
+		},
+	}
 }
 
 /* GET SECRET */
 pub async fn retrieve_secret_shares(
-    extract::Json(requested_secret): extract::Json<SecretGet>,
+	extract::Json(requested_secret): extract::Json<SecretGet>,
 ) -> impl IntoResponse {
-    let verified_req = requested_secret.verify_receive_data().await;
-    match verified_req {
-        Ok(data) => {
-            let file_path = NFT_DIR_PATH.to_owned() + &data.nft_id.to_string() + ".secret";
-            let mut file = std::fs::File::open(file_path).unwrap();
-            let mut nft_secret_share = Vec::<u8>::new();
+	let verified_req = requested_secret.verify_receive_data().await;
+	match verified_req {
+		Ok(data) => {
+			let file_path = NFT_DIR_PATH.to_owned() + &data.nft_id.to_string() + ".secret";
+			let mut file = std::fs::File::open(file_path).unwrap();
+			let mut nft_secret_share = Vec::<u8>::new();
 
-            file.read_to_end(&mut nft_secret_share).unwrap();
-            
-            println!("Secret shares of {} retrieved by {}",data.nft_id, requested_secret.account_address);
+			file.read_to_end(&mut nft_secret_share).unwrap();
 
-            axum::Json(SecretGetResponse {
-                status: 200,
-                cluster_id: 1,
-                secret_data: SecretData {
-                    nft_id: data.nft_id,
-                    data: nft_secret_share,
-                },
-            })
-        }
-        Err(err) => match err {
-            SecretError::DecodeError => todo!(),
-            
-            SecretError::SignatureInvalid => axum::Json(SecretGetResponse {
-                status: 400,
-                cluster_id: 1,
-                secret_data: SecretData {
-                    nft_id: 0,
-                    data: "Error Invalid Signature or NFT owner".as_bytes().to_vec(),
-                },
-            }),
+			println!(
+				"Secret shares of {} retrieved by {}",
+				data.nft_id, requested_secret.account_address
+			);
 
-            SecretError::OwnerInvalid => axum::Json(SecretGetResponse {
-                status: 400,
-                cluster_id: 1,
-                secret_data: SecretData {
-                    nft_id: 0,
-                    data: "Error Invalid Signature or NFT owner".as_bytes().to_vec(),
-                },
-            }),
-        },
-    }
+			axum::Json(SecretGetResponse {
+				status: 200,
+				cluster_id: 1,
+				secret_data: SecretData { nft_id: data.nft_id, data: nft_secret_share },
+			})
+		},
+		Err(err) => match err {
+			SecretError::DecodeError => todo!(),
+
+			SecretError::SignatureInvalid => axum::Json(SecretGetResponse {
+				status: 400,
+				cluster_id: 1,
+				secret_data: SecretData {
+					nft_id: 0,
+					data: "Error Invalid Signature or NFT owner".as_bytes().to_vec(),
+				},
+			}),
+
+			SecretError::OwnerInvalid => axum::Json(SecretGetResponse {
+				status: 400,
+				cluster_id: 1,
+				secret_data: SecretData {
+					nft_id: 0,
+					data: "Error Invalid Signature or NFT owner".as_bytes().to_vec(),
+				},
+			}),
+		},
+	}
 }
 
-/*  TEST */
+/* TEST */
 #[cfg(test)]
 mod test {
-    use super::*;
-    use hex_literal::hex;
-    use subxt::ext::sp_runtime::app_crypto::Ss58Codec;
+	use super::*;
+	use hex_literal::hex;
+	use subxt::ext::sp_runtime::app_crypto::Ss58Codec;
 
-    #[tokio::test]
-    async fn verification_test() {
-        /*
-        Secret phrase `pulse roof remain feel system fabric wolf travel intact patrol chest carbon` is account:
-        Secret seed:       0x7376f932ed87cefd1595709c6a2e3a10511b9e643723e65aead3ef1620c8d0b7
-        Public key (hex):  0x1c4a6fe4fe51c00cd8b5948a143f055b789050b99fd28d95095b542dd122370c
-        Public key (SS58): 5ChoJxKns4yyHeZg38U2hc8WYQ691oHzPJZtnayZXFyXvXET
-        Account ID:        0x1c4a6fe4fe51c00cd8b5948a143f055b789050b99fd28d95095b542dd122370c
-        SS58 Address:      5ChoJxKns4yyHeZg38U2hc8WYQ691oHzPJZtnayZXFyXvXET
-        */
+	#[tokio::test]
+	async fn verification_test() {
+		/*
+		Secret phrase `pulse roof remain feel system fabric wolf travel intact patrol chest carbon` is account:
+		Secret seed:       0x7376f932ed87cefd1595709c6a2e3a10511b9e643723e65aead3ef1620c8d0b7
+		Public key (hex):  0x1c4a6fe4fe51c00cd8b5948a143f055b789050b99fd28d95095b542dd122370c
+		Public key (SS58): 5ChoJxKns4yyHeZg38U2hc8WYQ691oHzPJZtnayZXFyXvXET
+		Account ID:        0x1c4a6fe4fe51c00cd8b5948a143f055b789050b99fd28d95095b542dd122370c
+		SS58 Address:      5ChoJxKns4yyHeZg38U2hc8WYQ691oHzPJZtnayZXFyXvXET
+		*/
 
-        /*
-        let kp = sr25519::Pair::from_seed(&hex!(
-            "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60"
-        ));
+		/*
+		let kp = sr25519::Pair::from_seed(&hex!(
+			"9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60"
+		));
 
-        let public = kp.public();
-        */
-        
-        let (kp, _) = sr25519::Pair::from_string_with_seed(
-            "pulse roof remain feel system fabric wolf travel intact patrol chest carbon",
-            None,
-        )
-        .unwrap();
+		let public = kp.public();
+		*/
 
-        
-        let sd = SecretData {
-            nft_id: 48384,
-            data: "This is one share of eight shares of secret!".into(),
-        };
+		let (kp, _) = sr25519::Pair::from_string_with_seed(
+			"pulse roof remain feel system fabric wolf travel intact patrol chest carbon",
+			None,
+		)
+		.unwrap();
 
-        let ser_sd: Vec<u8> = serialize(&sd).unwrap();
-        //println!("serialized secure data = {}", ser_sd);
+		let sd = SecretData {
+			nft_id: 48384,
+			data: "This is one share of eight shares of secret!".into(),
+		};
 
-        let sp = SecretPost {
-            //account_address: AccountId32::from(public),
-            account_address: AccountId32::from_ss58check(
-                "5ChoJxKns4yyHeZg38U2hc8WYQ691oHzPJZtnayZXFyXvXET",
-            )
-            .unwrap(),
-            secret_data: ser_sd.clone(),
-            signature: kp.sign(&ser_sd),
-        };
-        
-        //println!("signature = {:-?}", kp.sign(&ser_sd));
+		let ser_sd: Vec<u8> = serialize(&sd).unwrap();
+		//println!("serialized secure data = {}", ser_sd);
 
-        match sp.verify_receive_data().await {
-            Ok(secret) => println!("Secret is Valid!"),
+		let sp = SecretPost {
+			//account_address: AccountId32::from(public),
+			account_address: AccountId32::from_ss58check(
+				"5ChoJxKns4yyHeZg38U2hc8WYQ691oHzPJZtnayZXFyXvXET",
+			)
+			.unwrap(),
+			secret_data: ser_sd.clone(),
+			signature: kp.sign(&ser_sd),
+		};
 
-            Err(err) => match err {
-                SecretError::SignatureInvalid => println!("Signature Error!"),
+		//println!("signature = {:-?}", kp.sign(&ser_sd));
 
-                SecretError::OwnerInvalid => println!("Invalid Owner!"),
+		match sp.verify_receive_data().await {
+			Ok(secret) => println!("Secret is Valid!"),
 
-                SecretError::DecodeError => println!("Decode Error"),
-            },
-        }
-    }
+			Err(err) => match err {
+				SecretError::SignatureInvalid => println!("Signature Error!"),
+
+				SecretError::OwnerInvalid => println!("Invalid Owner!"),
+
+				SecretError::DecodeError => println!("Decode Error"),
+			},
+		}
+	}
 }
--- src/servers/http_server.rs
+++ src/servers/http_server.rs
@@ -1,21 +1,28 @@
 use axum::{
-    http::{HeaderValue, Method, StatusCode},
-    routing::{get, get_service, post},
-    Router,
+	http::{Method, StatusCode},
+	routing::{get, get_service, post},
+	Router,
 };
 
 use crate::server_common;
 use std::path::PathBuf;
-use tower_http::{cors::Any, cors::CorsLayer, services::ServeDir};
+use tower_http::{
+	cors::{Any, CorsLayer},
+	services::ServeDir,
+};
 
-use crate::chain::chain::{get_constant, get_nft_data_handler, rpc_query, submit_tx};
-use crate::chain::nft::{retrieve_secret_shares, store_secret_shares};
-use crate::keys::pgp_keys::{generate_key, get_public_key, get_public_key_url};
+use crate::{
+	chain::{
+		chain::{get_constant, get_nft_data_handler, rpc_query, submit_tx},
+		nft::{retrieve_secret_shares, store_secret_shares},
+	},
+	keys::pgp_keys::{generate_key, get_public_key, get_public_key_url},
+};
 
 /* HTTP Server */
 pub async fn http_server(port: &u16) {
-    let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
-    let cors = CorsLayer::new()
+	let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
+	let cors = CorsLayer::new()
         // allow `GET` and `POST` when accessing the resource
         .allow_methods([Method::GET, Method::POST])
         // allow requests from any origin
@@ -27,36 +34,33 @@ pub async fn http_server(port: &u16) {
         )*/
         ;
 
-    let http_app = Router::new()
-        .fallback(
-            get_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
-                .handle_error(|error: std::io::Error| async move {
-                    (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        format!("Unhandled internal error: {}", error),
-                    )
-                }),
-        )
-        .route("/", get(health_handler))
-        .layer(cors)
-        // TEST APIS
-        .route("/api/generateKey", get(generate_key))
-        .route("/api/getPublicKey", get(get_public_key))
-        .route("/api/getPublicKeyUrl", get(get_public_key_url))
-        .route("/api/getNFTData/:nft_id", get(get_nft_data_handler))
-        .route("/api/rpcQuery/:blocknumber", get(rpc_query))
-        .route("/api/submitTx/:amount", get(submit_tx))
-        // SECRET SHARING API
-        .route("/api/nft/storeSecretShares", post(store_secret_shares))
-        .route("/api/nft/retrieveSecretShares",get(retrieve_secret_shares),
-        );
+	let http_app = Router::new()
+		.fallback_service(
+			get_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
+				.handle_error(|error: std::io::Error| async move {
+					(
+						StatusCode::INTERNAL_SERVER_ERROR,
+						format!("Unhandled internal error: {}", error),
+					)
+				}),
+		)
+		.route("/", get(health_handler))
+		.layer(cors)
+		// TEST APIS
+		.route("/api/generateKey", get(generate_key))
+		.route("/api/getPublicKey", get(get_public_key))
+		.route("/api/getPublicKeyUrl", get(get_public_key_url))
+		.route("/api/getNFTData/:nft_id", get(get_nft_data_handler))
+		.route("/api/rpcQuery/:blocknumber", get(rpc_query))
+		.route("/api/submitTx/:amount", get(submit_tx))
+		// SECRET SHARING API
+		.route("/api/nft/storeSecretShares", post(store_secret_shares))
+		.route("/api/nft/retrieveSecretShares", get(retrieve_secret_shares));
 
-    server_common::serve(http_app, port).await;
+	server_common::serve(http_app, port).await;
 }
-
-/* Different Responses  */
 
 // TEXT
 async fn health_handler() -> &'static str {
-    "Server is running!\n"
+	"Server is running!\n"
 }
