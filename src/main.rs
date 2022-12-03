4bde01b3817397f890066814658bebbcf49496f0
Ubuntu <ubuntu@ns5019429.ip-15-235-119.net>
2022-12-03 13:37:05 +0000

    To have the right Signature, Using BTreeMap is critical for deserialization of Json request.
    Former-commit-id: 861c1dac68991176a141371d48b81a0b01b99747 [formerly e9aec3f84ae28a78a2837d5e8915c56fb01c6edd]
    Former-commit-id: 4552cdad975d5dfbf2bf87147c3d08fbd3eb9e93

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs::File, io::prelude::*};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing::{info, Level};
+use tracing_subscriber::{fmt, layer::SubscriberExt, Registry};
 
 mod backup;
 mod chain;
@@ -8,36 +9,14 @@ mod servers;
 use crate::servers::http_server;
 use std::env;
 
-use chrono::Local;
-use env_logger::Builder;
-use log::LevelFilter;
-
 /* MAIN */
 
 #[tokio::main(worker_threads = 4)]
 async fn main() {
-	Builder::new()
-		.format(|buf, record| {
-			writeln!(
-				buf,
-				"{} [{}] - {}",
-				Local::now().format("%Y-%m-%dT%H:%M:%S"),
-				record.level(),
-				record.args()
-			)
-		})
-		.filter(None, LevelFilter::Info)
-		.init();
-
 	let quote = generate_quote();
 
-	tracing_subscriber::registry()
-		.with(tracing_subscriber::EnvFilter::new(
-			std::env::var("RUST_LOG")
-				.unwrap_or_else(|_| "example_websockets=debug,tower_http=debug".into()),
-		))
-		.with(tracing_subscriber::fmt::layer())
-		.init();
+	let logger = Registry::default().with(fmt::Layer::default());
+	tracing::subscriber::set_global_default(logger).unwrap();
 
 	let args: Vec<String> = env::args().collect();
 	if args.len() < 1 {
@@ -51,28 +30,28 @@ async fn main() {
 
 fn generate_quote() -> Vec<u8> {
 	if !std::path::Path::new("/dev/attestation/user_report_data").exists() {
-		log::warn!("This is NOT inside an Enclave!");
+		info!("This is NOT inside an Enclave!");
 		return Vec::new()
 	}
 
 	let mut f1 = File::open("/dev/attestation/user_report_data").unwrap();
-	log::info!("This is inside Enclave!");
+	info!("This is inside Enclave!");
 
 	let mut f2 = File::open("/dev/attestation/attestation_type").unwrap();
 	let mut attest_type = String::new();
 	f2.read_to_string(&mut attest_type);
-	log::info!("attestation type is : {}", attest_type);
+	info!("attestation type is : {}", attest_type);
 
 	let write_zero = [0; 640];
 	f1.write_all(&write_zero);
 
-	log::info!("Reading The Quote ...");
+	info!("Reading The Quote ...");
 	let mut f3 = File::open("/dev/attestation/quote").unwrap();
 	let mut contents = vec![];
 	f3.read_to_end(&mut contents).unwrap();
 	//println!("{:-#?}",contents);
 
-	log::info!("Writing the Quote");
+	info!("Writing the Quote");
 	let mut f4 = File::create("/quote/enclave.quote").unwrap();
 	f4.write_all(&contents);
 
