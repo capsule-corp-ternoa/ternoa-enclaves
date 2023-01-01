1be12ce4b6d943203b0a439510161c1a36e3cffb
Ubuntu <ubuntu@dev-c1n1.ternoa.network>
2023-01-01 22:34:09 +0000

    Binary checksum verification, Define Errors and Return codes, Finalize deployment scripts
    Former-commit-id: 255222935bd9f0579820113bfdd4e6ad37ae69a0

--- src/servers/http_server.rs
+++ src/servers/http_server.rs
@@ -1,12 +1,12 @@
 use axum::{
+	extract::State,
 	http::{Method, StatusCode},
 	routing::{get, get_service, post},
 	Json, Router,
-	extract::State,
 };
 
 use serde_json::{json, Value};
-use sp_core::{Pair, crypto::Ss58Codec};
+use sp_core::{crypto::Ss58Codec, Pair};
 use std::time::SystemTime;
 
 use crate::servers::server_common;
@@ -22,20 +22,21 @@ use crate::chain::{
 	nft::{retrieve_secret_shares, store_secret_shares},
 };
 
-use crate::backup::admin::{backup_fetch_secrets, backup_push_secrets};
 use crate::attestation;
-
+use crate::backup::admin::{backup_fetch_secrets, backup_push_secrets};
 
 #[derive(Clone)]
 pub struct StateConfig {
 	pub owner_key: schnorrkel::Keypair,
 	pub enclave_key: sp_core::sr25519::Pair,
 	pub seal_path: String,
+	pub identity: String,
 }
 
 /* HTTP Server */
 pub async fn http_server(
 	port: &u16,
+	identity: &str,
 	account: &str,
 	certfile: &str,
 	keyfile: &str,
@@ -44,11 +45,19 @@ pub async fn http_server(
 	let account_keys: Vec<&str> = account.split("_").collect();
 	let private_bytes = hex::decode(account_keys[0]).expect("Error reading account data");
 	let public_bytes = hex::decode(account_keys[1]).expect("Error reading account data");
-	let account_pair = schnorrkel::Keypair {secret: schnorrkel::SecretKey::from_bytes(&private_bytes).unwrap(), public: schnorrkel::PublicKey::from_bytes(&public_bytes).unwrap() };
-	
+	let account_pair = schnorrkel::Keypair {
+		secret: schnorrkel::SecretKey::from_bytes(&private_bytes).unwrap(),
+		public: schnorrkel::PublicKey::from_bytes(&public_bytes).unwrap(),
+	};
+
 	let (enclave_pair, _) = sp_core::sr25519::Pair::generate();
-	
-	let state_config = StateConfig { owner_key: account_pair, enclave_key: enclave_pair, seal_path: seal_path.to_owned() };
+
+	let state_config = StateConfig {
+		owner_key: account_pair,
+		enclave_key: enclave_pair,
+		seal_path: seal_path.to_owned(),
+		identity: identity.to_string(),
+	};
 
 	let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
 
@@ -71,9 +80,6 @@ pub async fn http_server(
 		.route("/health", get(get_health_status))
 		.layer(cors)
 		// TEST APIS
-		//.route("/api/generateKey", get(generate_key))
-		//.route("/api/getPublicKey", get(get_public_key))
-		//.route("/api/getPublicKeyUrl", get(get_public_key_url))
 		.route("/api/getNFTData/:nft_id", get(get_nft_data_handler))
 		.route("/api/rpcQuery/:blocknumber", get(rpc_query))
 		.route("/api/submitTx/:amount", get(submit_tx))
@@ -89,17 +95,23 @@ pub async fn http_server(
 }
 
 /*  -------------Handlers------------- */
-async fn get_health_status(State(state): State<StateConfig>,) -> Json<Value> {
-	let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+async fn get_health_status(State(state): State<StateConfig>) -> Json<Value> {
+	let time: chrono::DateTime<chrono::offset::Utc> = SystemTime::now().into();
+
+	let quote_vec = attestation::ra::generate_quote();
+
+	let operator =
+		sp_core::sr25519::Public::from_raw(state.owner_key.public.to_bytes()).to_ss58check();
+	let checksum = self_check();
 
 	Json(json!({
 		"status": 200,
-		"date": time.as_secs(),
-		"description": "SGX server healthy!".to_string(),
+		"date": time.format("%Y-%m-%d %H:%M:%S").to_string(),
+		"description": "SGX server is running!".to_string(),
 		"encalve_address": state.enclave_key.public().to_ss58check(),
-		"operator_address": sp_core::sr25519::Public::from_raw(state.owner_key.public.to_bytes()).to_ss58check(),
-		"binary_hash" : self_check(),
-		"quote": attestation::ra::generate_quote(),
+		"operator_address": operator,
+		"binary_hash" : checksum,
+		"quote": quote_vec,
 	}))
 }
 
@@ -124,12 +136,18 @@ fn self_check() -> Result<String, String> {
 	let bytes = std::fs::read(binary_path.clone()).unwrap();
 	let hash = sha256::digest(bytes.as_slice());
 
-	binary_path.pop(); // binary name
-	binary_path.pop(); // release
-	binary_path.pop(); // target
-	binary_path.push("SHA256");
-	let binary_hash = std::fs::read_to_string(binary_path.clone())
-		.expect(&format!("path not found : {}", binary_path.clone().to_str().unwrap()));
+	binary_path.pop(); // remove binary name
+	binary_path.push("checksum");
+
+	let binary_hash = std::fs::read_to_string(binary_path.clone()).expect(&format!(
+		"Binary-checksum path not found : {}",
+		binary_path.clone().to_str().unwrap()
+	));
+
+	let binary_hash = binary_hash
+		.strip_suffix("\r\n")
+		.or(binary_hash.strip_suffix("\n"))
+		.unwrap_or(&binary_hash);
 
 	if binary_hash != hash {
 		tracing::error!("Binary hash doesn't match!");
@@ -138,5 +156,4 @@ fn self_check() -> Result<String, String> {
 		tracing::info!("Binary hash match : {}", hash);
 		return Ok(hash);
 	}
-	
 }
