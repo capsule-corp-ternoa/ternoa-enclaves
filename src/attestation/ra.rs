8ddb4379966890203ecd1d88acd1f08f280532cd
kalanamithm <kalanamith@outlook.com>
2023-03-01 15:41:41 +1300

    attestation ra.rs refactoring
    Former-commit-id: e9755c671ec58c5cb36cc6add040904375460258 [formerly 1dd21bdc6ac0830986cec54ec6636cb433fc2003] [formerly 9471014fb4cf6a6eeda70f7b3ea10d2b338c96d7 [formerly 144ff3ddf4b335285ffbccf5fc28adeed805a4f5]] [formerly 7193a09fd8a3f742fedf7c1187953bd89d13f9c7 [formerly 0d1098609992a576219a5000354cb4ae420ae856] [formerly c067759ad7001325a98c18e564d9322c3c4b30ec [formerly 486dcc09c74ce3abdce83409a6e9aa6a28c47283]]] [formerly 388e00fc7bc7e3b0613e7d0274dffa798916f3b2]
    Former-commit-id: 731a3a77dd093aa657dedac3c41538719328135e
    Former-commit-id: 26a03cc06a291a9c04c458c9bc3b8fdde1267387 [formerly 09af3e1c2d0761f114b55ecc90445e32f9ff2274] [formerly b281163b66059680930758f9ac3798f3ce058d2c [formerly d5421cd18c52c4ff3d9d2dab7e124c89d8bf1f25]]
    Former-commit-id: f81c56d08e90c245647b1af464e3e40af4b06bd7 [formerly f5ca9984b93a8befadb262dcef6ce39af30ad92e]
    Former-commit-id: 35247921f8d1c5c1e9441a1b1ee3693eb05b95a5

--- src/attestation/ra.rs
+++ src/attestation/ra.rs
@@ -100,44 +100,4 @@ fn is_user_report_data_exist(file_path: Option<String>) -> bool {
 		None => { Path::new("/dev/attestation/user_report_data").exists()}
 		Some(_) => Path::new(file_path.unwrap().as_str()).exists()
 	}
-
-	// if Some(file_path.clone()) {
-	// 	Path::new(file_path.unwrap().as_str()).exists()
-	// } else {
-	// 	Path::new("/dev/attestation/user_report_data").exists()
-	// }
 }
-
-// pub fn _generate_quote() -> Vec<u8> {
-// 	if !std::path::Path::new("/dev/attestation/user_report_data").exists() {
-// 		info!("This is NOT inside an Enclave!");
-// 		return "This is NOT inside an Enclave!".as_bytes().to_vec()
-// 	}
-//
-// 	let mut f1 = OpenOptions::new()
-// 		.write(true)
-// 		.open("/dev/attestation/user_report_data")
-// 		.unwrap(); // TODO: manage unwrap()
-// 	info!("This is inside Enclave!");
-//
-// 	let mut f2 = File::open("/dev/attestation/attestation.attestation_type").unwrap(); // TODO: manage unwrap()
-// 	let mut attest_type = String::new();
-// 	f2.read_to_string(&mut attest_type).unwrap(); // TODO: manage unwrap()
-// 	info!("attestation type is : {}", attest_type);
-//
-// 	let write_zero = [0u8; 64];
-// 	f1.write(&write_zero)
-// 		.expect("Error writing to /dev/attestation/user_report_data"); // TODO: manage expect()
-//
-// 	info!("Reading The Quote ...");
-// 	let mut f3 = File::open("/dev/attestation/quote").unwrap(); // TODO: manage unwrap()
-// 	let mut contents = vec![];
-// 	f3.read_to_end(&mut contents).unwrap(); // TODO: manage unwrap()
-// 										//println!("{:-#?}",contents);
-//
-// 	info!("Dumping the Quote");
-// 	let mut f4 = File::create("/quote/enclave.quote").unwrap(); // TODO: manage unwrap()
-// 	f4.write_all(&contents).unwrap(); // TODO: manage unwrap()
-//
-// 	return contents
-// }
