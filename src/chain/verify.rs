6831ff5694b99d0b66a1aa3c079ab8db1cd07725
tapiocaboy <kalanamith@outlook.com>
2023-03-16 22:39:50 +1300

    refactoring
    Former-commit-id: 4818796a1fdb2ab99c265f3e4e95ecefceaae611

--- src/chain/verify.rs
+++ src/chain/verify.rs
@@ -533,7 +533,6 @@ pub async fn verify_requester_type(
 	owner: AccountId32,
 	requester_type: RequesterType,
 ) -> bool {
-
 	match AccountId32::from_str(&requester_address) {
 		Ok(converted_requester_address) => {
 			match requester_type {
