183f5b53768be7fe80272aa879974a6e844e7931
Amin Razavi <nonlinear_dynamic@yahoo.com>
2023-04-15 23:56:55 +0400

    Update fetch-release.sh
    Former-commit-id: a4fbeb11830ace44bda26b0e0491bc9cc5349b33

--- scripts/fetch-release.sh
+++ scripts/fetch-release.sh
@@ -5,11 +5,10 @@ else
 read -p "Please enter your github authentication-token to access private repository : " AUTHTOKEN
 fi
 
-CURL="curl -H 'Authorization: token $1' \
-      https://api.github.com/repos/capsule-corp-ternoa/sgx_server/releases"; \
+CURL="curl -H 'Authorization: token $AUTHTOKEN' https://api.github.com/repos/capsule-corp-ternoa/sgx_server/releases"
 
-BINARY=$(eval "$CURL/tags/master" | jq .assets[0].id); \
-SIGNATURE=$(eval "$CURL/tags/master" | jq .assets[1].id); \
+BINARY=$(eval "$CURL/tags/master" | jq .assets[0].id)
+SIGNATURE=$(eval "$CURL/tags/master" | jq .assets[1].id)
     
 eval "$CURL/assets/$BINARY -LJOH 'Accept: application/octet-stream'"
 eval "$CURL/assets/$SIGNATURE -LJOH 'Accept: application/octet-stream'"
