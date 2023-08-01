6d245bbcea6c38acc2ece1e4b4b85390d175aa0c
Zorvan <amin@capsule-corp.io>
2023-08-01 10:38:44 +0000

    "test zip response for sync keyshares"
    Former-commit-id: 3132e6c2dee35c91c35893223ab782900b99361c
    Former-commit-id: a9baad76c87f139e4d8b75db7d0e2d48438e45df

--- src/backup/sync.rs
+++ src/backup/sync.rs
@@ -330,136 +330,123 @@ pub async fn sync_keyshares(
 	// TODO [future reliability] check nftids , is empty, are in range, ...
 
 	// Create a client
-	let client = reqwest::Client::builder()
-		// TODO : only for dev
-		.danger_accept_invalid_certs(true)
-		.https_only(false)
-		// .min_tls_version(if cfg!(any(feature = "mainnet", feature = "alphanet")) {
-		// 	tls::Version::TLS_1_3
-		// } else {
-		// 	tls::Version::TLS_1_0
-		// })
-		.build()
-		.unwrap();
+	// let client = reqwest::Client::builder()
+	// 	// TODO : only for dev
+	// 	.danger_accept_invalid_certs(true)
+	// 	.https_only(false)
+	// 	// .min_tls_version(if cfg!(any(feature = "mainnet", feature = "alphanet")) {
+	// 	// 	tls::Version::TLS_1_3
+	// 	// } else {
+	// 	// 	tls::Version::TLS_1_0
+	// 	// })
+	// 	.connection_verbose(true)
+	// 	.build()
+	// 	.unwrap();
 
-	let google_request_url = "https://www.google.com";
-
-	let google_response = match client
-		.get(google_request_url.clone())
-		.send()
-		.await {
-			Ok(res) => res,
-			Err(err) => {
-				let message = format!("Error getting health-check response from the enclave requesting for syncing : {} : {:?}", google_request_url, err);
-				return error_handler(message, &state).await.into_response();
-			}, 
-		};
-
-	debug!("Google response {:?}", google_response);
-
-	let mut enclave_url = requester.1.enclave_url.clone();
-	while enclave_url.ends_with('/') { enclave_url.pop(); }
+	// let mut enclave_url = requester.1.enclave_url.clone();
+	// while enclave_url.ends_with('/') { enclave_url.pop(); }
 	
-	let health_request_url = enclave_url.clone() + "/api/health";
+	// let health_request_url = enclave_url.clone() + "/api/health";
 	
-	debug!("\t - SYNC KEYSHARES : HEALTH-CHECK the requester {}", health_request_url);
-	let health_response = match client
-		.get(health_request_url.clone())
-		.send()
-		.await {
-			Ok(res) => res,
-			Err(err) => {
-				let message = format!("Error getting health-check response from the enclave requesting for syncing : {} : {:?}", health_request_url, err);
-				return error_handler(message, &state).await.into_response();
-			}, 
-		};
-	// Analyze the Response
-	let health_status = health_response.status();
+	// debug!("\t - SYNC KEYSHARES : HEALTH-CHECK the requester {}", health_request_url);
+	// let health_response = match client
+	// 	.get(health_request_url.clone())
+	// 	.fetch_mode_no_cors()
+	// 	.send()
+	// 	.await {
+	// 		Ok(res) => res,
+	// 		Err(err) => {
+	// 			let message = format!("Error getting health-check response from the enclave requesting for syncing : {} : {:?}", health_request_url, err);
+	// 			return error_handler(message, &state).await.into_response();
+	// 		}, 
+	// 	};
+	// // Analyze the Response
+	// let health_status = health_response.status();
 
-	// TODO [decision] : Should it be OK or Synching? Solution = (Specific StatusCode for Wildcard)
+	// // TODO [decision] : Should it be OK or Synching? Solution = (Specific StatusCode for Wildcard)
 
-	if health_status != StatusCode::OK {
-		let message = format!(
-			"Synch Keyshares : Healthcheck : requester enclave {} is not ready for synching",
-			requester.1.enclave_url
-		);
-		return error_handler(message, &state).await.into_response();
-	}
+	// if health_status != StatusCode::OK {
+	// 	let message = format!(
+	// 		"Synch Keyshares : Healthcheck : requester enclave {} is not ready for synching",
+	// 		requester.1.enclave_url
+	// 	);
+	// 	return error_handler(message, &state).await.into_response();
+	// }
 
-	let health_body: HealthResponse = match health_response.json().await {
-		Ok(body) => body,
-		Err(e) => {
-			let message = format!(
-				"Synch Keyshares : Healthcheck : can not deserialize the body : {} : {:?}",
-				requester.1.enclave_url, e
-			);
-			error!(message);
+	// let health_body: HealthResponse = match health_response.json().await {
+	// 	Ok(body) => body,
+	// 	Err(e) => {
+	// 		let message = format!(
+	// 			"Synch Keyshares : Healthcheck : can not deserialize the body : {} : {:?}",
+	// 			requester.1.enclave_url, e
+	// 		);
+	// 		error!(message);
 			
-			HealthResponse {
-				block_number: 0,
-				sync_state: "0".to_string(),
-				version: "0.0".to_string(),
-				description: "Error".to_string(),
-				enclave_address: "0000".to_string(),
-			}
-			//return error_handler(message, &state).await.into_response();
-		},
-	};
+	// 		HealthResponse {
+	// 			block_number: 0,
+	// 			sync_state: "0".to_string(),
+	// 			version: "0.0".to_string(),
+	// 			description: "Error".to_string(),
+	// 			enclave_address: "0000".to_string(),
+	// 		}
+	// 		//return error_handler(message, &state).await.into_response();
+	// 	},
+	// };
 
-	debug!(
-		"Fetch Keyshares : Health-Check Result for url : {}, Status: {:?}, \n body: {:#?}",
-		requester.1.enclave_url, health_status, health_body
-	);
+	// debug!(
+	// 	"Fetch Keyshares : Health-Check Result for url : {}, Status: {:?}, \n body: {:#?}",
+	// 	requester.1.enclave_url, health_status, health_body
+	// );
 
-	debug!("\t - SYNC KEYSHARES : REQEST QUOTE");
-	let quote_request_url = enclave_url.clone() + "/api/quote";
+	// debug!("\t - SYNC KEYSHARES : REQEST QUOTE");
+	// let quote_request_url = enclave_url.clone() + "/api/quote";
 
-	let quote_response = match
-		client.get(quote_request_url).send().await {
-			Ok(resp) => resp,
-			Err(err) => {
-				let message = format!("Error reading quote from the enclave requesting for syncing : {:?}",err);
-				return error_handler(message, &state).await.into_response();
-			}
+	// let quote_response = match
+	// 	client.get(quote_request_url).send().await {
+	// 		Ok(resp) => resp,
+	// 		Err(err) => {
+	// 			let message = format!("Error reading quote from the enclave requesting for syncing : {:?}",err);
+	// 			return error_handler(message, &state).await.into_response();
+	// 		}
 
-		};
+	// 	};
 
-	let quote_body: QuoteResponse = match quote_response.json().await {
-		Ok(body) => body,
-		Err(e) => {
-			let message = format!(
-				"Synch Keyshares : Healthcheck : can not deserialize the body : {} : {:?}",
-				requester.1.enclave_url, e
-			);
-			return error_handler(message, &state).await.into_response();
-		},
-	};
+	// let quote_body: QuoteResponse = match quote_response.json().await {
+	// 	Ok(body) => body,
+	// 	Err(e) => {
+	// 		let message = format!(
+	// 			"Synch Keyshares : Healthcheck : can not deserialize the body : {} : {:?}",
+	// 			requester.1.enclave_url, e
+	// 		);
+	// 		return error_handler(message, &state).await.into_response();
+	// 	},
+	// };
 
-	debug!(
-		"Fetch Keyshares : Quote Result for url : {} is {:#?}",
-		requester.1.enclave_url, quote_body
-	);
+	// debug!(
+	// 	"Fetch Keyshares : Quote Result for url : {} is {:#?}",
+	// 	requester.1.enclave_url, quote_body
+	// );
 
-	let attest_response = match client
-		.post("https://dev-c1n1.ternoa.network:9100/attest")
-		.body(quote_body.data)
-		.header(header::CONTENT_TYPE, "application/json")
-		.send()
-		.await {
-			Ok(resp) => resp,
-			Err(err) => {
-				let message = format!(
-					"Synch Keyshares : Attestation : can not get response from attestation server : {:?}", err);
-				return error_handler(message, &state).await.into_response();
-			},
-		};
+	// let attest_response = match client
+	// 	.post("https://dev-c1n1.ternoa.network:9100/attest")
+	// 	.body(quote_body.data)
+	// 	.header(header::CONTENT_TYPE, "application/json")
+	// 	.send()
+	// 	.await {
+	// 		Ok(resp) => resp,
+	// 		Err(err) => {
+	// 			let message = format!(
+	// 				"Synch Keyshares : Attestation : can not get response from attestation server : {:?}", err);
+	// 			return error_handler(message, &state).await.into_response();
+	// 		},
+	// 	};
 
-	// TODO [development : attestation] : extract user_data and verify the signature and block_number
-	debug!(
-		"Fetch Keyshares : Attestation Result for url : {} is {:#?}",
-		requester.1.enclave_url,
-		match attest_response.text().await { Ok(resp) => resp, Err(e) => format!("Error getting attestation response {:?}", e)}
-	);
+	// // TODO [development : attestation] : extract user_data and verify the signature and block_number
+	// debug!(
+	// 	"Fetch Keyshares : Attestation Result for url : {} is {:#?}",
+	// 	requester.1.enclave_url,
+	// 	match attest_response.text().await { Ok(resp) => resp, Err(e) => format!("Error getting attestation response {:?}", e)}
+	// );
 
 	let backup_file = "/temporary/backup.zip".to_string();
 	//let counter = 1;
