8eb4a54b9f3225fe48403a4e335db780f977357c
zorvan <amin@capsule-corp.io>
2023-09-24 10:29:02 +0000

    clear deployment, clear manifest, clear artifacts
    Former-commit-id: 2be3a5bc0bce455abf6a66ed17eca7b66bfc7e65 [formerly a17dbe76c29be66400be71da987ee2fab1d7116b] [formerly 686330f3d25fa863233cae33cb00923c56b7fc4c [formerly 86f5bc11b14138188202bf644efca0f494607a2f]] [formerly 2ad2965bde7a757a941e09b6c1f951a42b93748b [formerly ff66410265bd56c07a41bc9785833482fdc7f819] [formerly 17e2f3b6baf6b5d710eb75ebcf4e6dd61f384ccf [formerly d5d4735a2bdddb7636de0e584b172ccf867c3b1e]]] [formerly 151cd8f00e2a08ad19fde06bcb7305d23c1f2ec4 [formerly d255c3492d408260344b65386f85bdc03d014099] [formerly 86bc9b562f6502bf2e961082c9aa6f0445696484 [formerly c5f2e6ad6dcea41fca7036aaaa46f347f9d11e5e]] [formerly 9e7335dceab78ea43a3120cbbaf78efbf4242f3f [formerly bcf61200bfcbdb8aeb9c029160d6e76ea2964f33] [formerly 843658b87fe7fbf006c3fbe43fbe2d391f396e0d [formerly fb0542a93298325359ed0bb49df80c1261a1b318]]]] [formerly 9dc9bda5498b2b450385b11bd053bbc71c42052d [formerly 13c677d56f5d5a5b6faead4576d53d9905560072] [formerly 4b34fb104c41243c1359e07c69578f512c82bb1b [formerly b0b464186e7269558602eb4bb85fb9a7cda13647]] [formerly 6344295289fa3c0fa5be3950969e3c282bf2c6a2 [formerly b60c2134e03bbe27a591a18ec6f546137c41e668] [formerly 693777570d7fa1f7272a4c29cb735291b6600860 [formerly 507165d06cf3e0689e1ad031011485d9ae1ca483]]] [formerly d969b61881a70aadddd3a5966dbaf15aae768e77 [formerly 1492249b01a7fba97e6df5051669a03d147db1d5] [formerly 0831f708ffb984fc7b7549eaca1c9cd0023de7b4 [formerly b08768728ac5ffeed2552e39ba816b9a4904f202]] [formerly cbbf008c20eca5b74125574e9753eec20c8ad1f1 [formerly bbc1842bb63efefb4e7cf575dfce232fa220342f] [formerly 001dcdf1aa3ee9016d95c94903d20f33483bae6d [formerly c2fcb582fc384ba3fbcf8bb39adf60f01bd8180e]]]]]
    Former-commit-id: 2be3a5bc0bce455abf6a66ed17eca7b66bfc7e65 [formerly a17dbe76c29be66400be71da987ee2fab1d7116b] [formerly 686330f3d25fa863233cae33cb00923c56b7fc4c [formerly 86f5bc11b14138188202bf644efca0f494607a2f]] [formerly 2ad2965bde7a757a941e09b6c1f951a42b93748b [formerly ff66410265bd56c07a41bc9785833482fdc7f819] [formerly 17e2f3b6baf6b5d710eb75ebcf4e6dd61f384ccf [formerly d5d4735a2bdddb7636de0e584b172ccf867c3b1e]]] [formerly 151cd8f00e2a08ad19fde06bcb7305d23c1f2ec4 [formerly d255c3492d408260344b65386f85bdc03d014099] [formerly 86bc9b562f6502bf2e961082c9aa6f0445696484 [formerly c5f2e6ad6dcea41fca7036aaaa46f347f9d11e5e]] [formerly 9e7335dceab78ea43a3120cbbaf78efbf4242f3f [formerly bcf61200bfcbdb8aeb9c029160d6e76ea2964f33] [formerly 843658b87fe7fbf006c3fbe43fbe2d391f396e0d [formerly fb0542a93298325359ed0bb49df80c1261a1b318]]]]
    Former-commit-id: ccd09480dc9a7cd8d227e4a414900cbc56dbca16 [formerly 056569085550fb22463ba941e758080985fa1776] [formerly 918de3a13c1ce2434e10be6ff1764af131a0e9d1 [formerly dbf490947484f053cebc127b65effc82f37d752e]] [formerly 9c1a24cef46f6293eca3afd062f666c6822d1d97 [formerly 1f7bb483cff294815d3418ad2910c9ab9096645f] [formerly 65ff6ee769b9443c15ce2b2ff7cff8b34385b09e [formerly 7f1fb0da677ebd15f6c6587e02855c9579b2e529]]]
    Former-commit-id: d1a91beeecfa7a6db6c9d8d9c38e75f08bc3291c [formerly ac24b910b8eeeef0f00e105dd7e7d8ee75b45349] [formerly 42707a286fb0054aa25ab58604db69ebf6676e8e [formerly 260150e44d1e8219f3f69eeb7f2681eadf4f33ea]]
    Former-commit-id: a6286890ae2869783c6d6718aa736a8f371f6c89 [formerly 1994c501c4022fa9154f8c292a4682fb2eba8e67]
    Former-commit-id: eade1b66f5bcf60c26b3ab698f8dd75f56179f9b

---
+++ deployment/ternoa_enclave/artifacts/get_metadata.sh
@@ -0,0 +1,4 @@
+subxt metadata --url wss://alphanet.ternoa.com:443 > ternoa_alphanet.scale &
+subxt metadata --url wss://mainnet.ternoa.network:443 > ternoa_mainnet.scale &
+subxt metadata --url wss://dev-1.ternoa.network:443 > ternoa_dev1.scale &
+subxt metadata --url wss://dev-0.ternoa.network:443 > ternoa_dev0.scale &
