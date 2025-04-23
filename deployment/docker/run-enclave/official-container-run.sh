8592f7e4a3afc9a674db42f664ac943be65d3646
Mohsan Riaz <bahrianboy@gmail.com>
2025-04-23 16:36:52 +0500

    added rpc url

--- deployment/docker/run-enclave/official-container-run.sh
+++ deployment/docker/run-enclave/official-container-run.sh
@@ -1,6 +1,7 @@
-ENCLAVE_VERSION=v0.5.1-alphanet \
+ENCLAVE_VERSION=v0.5.1-mainnet \
 ENCLAVE_DNS=8.8.8.8 \
 ENCLAVE_DOMAIN=enclave.newdomain.me \
 ENCLAVE_PORT=9000 \
-ENCLAVE_VERBOSITY=3 \
+ENCLAVE_VERBOSE=3 \
+RPCNODE=wss://mainnet.ternoa.network \
 docker-compose up -d
